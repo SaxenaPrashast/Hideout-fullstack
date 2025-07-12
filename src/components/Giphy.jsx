c5e31479aec61b9a68bacb58c3061a5d76317bc1
SaxenaPrashast <Saxenaprashast2003@gmail.com>
2025-07-12 23:32:29 +0530

    working on gif

--- src/components/Giphy.jsx
+++ src/components/Giphy.jsx
@@ -1,10 +1,24 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import {Grid} from '@giphy/react-components';
+import { GiphyFetch } from "@giphy/js-fetch-api";
+const apiKey = import.meta.env.VITE_GIPHY_API_KEY;
 
+import _ from 'lodash'
+
+const gf = new GiphyFetch(apiKey)
 function Giphy() {
     const gridRef = useRef(null);
     const [isloading , setisloading] = useState(false);
     const [value, setvalue ]= useState("");
     const [error, seterror ]=useState(false);
+    const fetchGifs = async(offset)=>{
+      return gf.search(value,{offset,limit: 10});
+    }
+
+    useEffect(()=>{
+
+    },[]);
+
   return (
     <div ref={gridRef} className='w-full'>
         <input type="text" placeholder='Search for Gif.... ' className='border border-stroke rounded-md p-2 w-full mb-2 outline-none mt-3' value={value} onChange={(e)=>{
@@ -13,7 +27,7 @@ function Giphy() {
 
         {isloading && <p>Loading GIFs...</p>}
         {error && <p className='text-red'>Error: {error}</p>}
-         <div></div>
+         <div className="h-48 overflow-auto no-scrollbar "></div>
     </div>
   )
 }
